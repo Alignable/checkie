11a842d71e60aed2c495728907d03d7b1686eebe
Aiden Cahill <aiden@alignable.com>
2026-02-26 16:30:38 -0500

    no dry run

--- lib/checkie/runner.rb
+++ lib/checkie/runner.rb
@@ -7,7 +7,7 @@ class Checkie::Runner
   # Action is the pull request action type
   def run(url, action)
     fetcher = Checkie::Fetcher.new(url)
-    poster = Checkie::Poster.new(fetcher.details, dry_run: true)
+    poster = Checkie::Poster.new(fetcher.details, dry_run: false)
 
     if action == "synchronize" || action == "opened"
       data = fetcher.fetch_files
